12508586e463555fca60650884151c2d80024140
Nathan <natemi1010@gmail.com>
2019-11-13 10:13:37 +0000

    [0.1.0] - First minor release for testing purposes

---
+++ Runtime/Extensions/ListExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace DapperDino.DapperTools
+{
+    public static class ListExtensions
+    {
+        public static T First<T>(this List<T> list) => list[0];
+        public static T Last<T>(this List<T> list) => list[list.Count - 1];
+    }
+}
---
+++ Runtime/Extensions/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace DapperDino.DapperTools
+{
+    public static class StringExtensions
+    {
+        public static string AsColour(this string text, Color colour)
+        {
+            return $"<color=#{ColorUtility.ToHtmlStringRGB(colour)}>{text}</color>";
+        }
+    }
+}
