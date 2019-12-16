df50c1b8f4faad47925d39bf4523cc16e24f603c
Botaemic <dbot@botaemic.com>
2019-12-16 10:07:36 +0100

    Added more Transform extensions
    Added more Transform extensions and tests for transformExtensions.cs

--- Runtime/Extensions/TransformExtensions.cs
+++ Runtime/Extensions/TransformExtensions.cs
@@ -15,5 +15,104 @@ namespace DapperDino.DapperTools.Extensions
                 Object.Destroy(child.gameObject);
             }
         }
+
+        /// <summary>
+        /// Return a Vector3, the direction from source to destination
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="destination"></param>
+        /// <returns></returns>
+        public static Vector3 DirectionTo(this Transform source, Vector3 destination)
+        {
+            return source.position.DirectionTo(destination);
+        }
+
+        /// <summary>
+        /// Return a normalized Vector3, the direction from source to destination
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="destination"></param>
+        /// <returns></returns>
+        public static Vector3 NormalDirectionTo(this Transform source, Vector3 destination)
+        {
+            return source.position.NormalDirectionTo(destination);
+        }
+
+        /// <summary>
+        /// Return a Vector3, the direction from source to destination
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="destination"></param>
+        /// <returns></returns>
+        public static Vector3 DirectionTo(this Transform source, Transform destination)
+        {
+            return source.DirectionTo(destination.position);
+        }
+
+        /// <summary>
+        /// Return a normalized Vector3, the direction from source to destination
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="destination"></param>
+        /// <returns></returns>
+        public static Vector3 NormalDirectionTo(this Transform source, Transform destination)
+        {
+            return source.NormalDirectionTo(destination.position);
+        }
+
+        /// <summary>
+        /// Return a Vector3, the direction from source to destination
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="destination"></param>
+        /// <returns></returns>
+        public static Vector3 DirectionTo(this Transform source, GameObject destination)
+        {
+            return source.DirectionTo(destination.transform.position);
+        }
+
+        /// <summary>
+        /// Return a normalized Vector3, the direction from source to destination
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="destination"></param>
+        /// <returns></returns>
+        public static Vector3 NormalDirectionTo(this Transform source, GameObject destination)
+        {
+            return source.NormalDirectionTo(destination.transform.position);
+        }
+
+        /// <summary>
+        /// Return a float. the distance between to points
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="destination"></param>
+        /// <returns></returns>
+        public static float DistanceTo(this Transform source, Vector3 destination)
+        {
+            return source.position.DistanceTo(destination);
+        }
+
+        /// <summary>
+        /// Return a float. the distance between to points
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="destination"></param>
+        /// <returns></returns>
+        public static float DistanceTo(this Transform source, Transform destination)
+        {
+            return source.position.DistanceTo(destination.position);
+        }
+
+        /// <summary>
+        /// Return a float. the distance between to points
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="destination"></param>
+        /// <returns></returns>
+        public static float DistanceTo(this Transform source, GameObject destination)
+        {
+            return source.DistanceTo(destination.transform);
+        }
     }
 }
---
+++ Tests/Runtime/Extensions/TransformExtensionsTests.cs
@@ -0,0 +1,87 @@
+﻿using DapperDino.DapperTools.Extensions;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace DapperDino.DapperTools.Tests.Extensions
+{
+    public class TransformExtensionsTests
+    {
+        [Test]
+        public void TransformDirectionTo_Vector3_Test()
+        {
+
+            var source = new GameObject();
+            source.transform.position = new Vector3(4f,4f,4f);
+            var destination = new Vector3(1f, 1f, 1f);
+            
+            var fetch   = source.transform.DirectionTo(destination);
+            var correct = (destination - source.transform.position);
+            Assert.AreEqual(correct, fetch);
+        }
+
+        [Test]
+        public void TransformNormalDirectionTo_Vector3_Test()
+        {
+
+            var source = new GameObject();
+            source.transform.position = new Vector3(4f, 4f, 4f);
+            var destination = new Vector3(1f, 1f, 1f);
+
+            var fetch = source.transform.NormalDirectionTo(destination);
+            var correct = (destination - source.transform.position).normalized;
+            Assert.AreEqual(correct, fetch);
+        }
+
+        [Test]
+        public void TransformDirectionTo_Transform_Test()
+        {
+            var source = new GameObject();
+            source.transform.position = new Vector3(4f, 4f, 4f);
+            var destination = new GameObject();
+            destination.transform.position = new Vector3(1f, 1f, 1f);
+
+            var fetch   = source.transform.DirectionTo(destination.transform);
+            var correct = (destination.transform.position - source.transform.position);
+            Assert.AreEqual(correct, fetch);
+        }
+
+        [Test]
+        public void TransformNormalDirectionTo_Transform_Test()
+        {
+            var source = new GameObject();
+            source.transform.position = new Vector3(4f, 4f, 4f);
+            var destination = new GameObject();
+            destination.transform.position = new Vector3(1f, 1f, 1f);
+
+            var fetch = source.transform.NormalDirectionTo(destination.transform);
+            var correct = (destination.transform.position - source.transform.position).normalized;
+            Assert.AreEqual(correct, fetch);
+        }
+
+        [Test]
+        public void TransformDirectionTo_GameObject_Test()
+        {
+            var source = new GameObject();
+            source.transform.position = new Vector3(4f, 4f, 4f);
+            var destination = new GameObject();
+            destination.transform.position = new Vector3(1f, 1f, 1f);
+            
+            var fetch   = source.transform.DirectionTo(destination);
+            var correct = (destination.transform.position - source.transform.position);
+            Assert.AreEqual(correct, fetch);
+        }
+
+        [Test]
+        public void TransformNormalDirectionTo_GameObject_Test()
+        {
+            var source = new GameObject();
+            source.transform.position = new Vector3(4f, 4f, 4f);
+            var destination = new GameObject();
+            destination.transform.position = new Vector3(1f, 1f, 1f);
+
+            var fetch = source.transform.NormalDirectionTo(destination);
+            var correct = (destination.transform.position - source.transform.position).normalized;
+            Assert.AreEqual(correct, fetch);
+        }
+    }
+}
